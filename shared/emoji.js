3a4515986e907a7cdec15e4c51b823bf871c6bc4
ZeroX0368 <dabibanban@gmail.com>
2025-06-15 06:34:38 +0700

    Create emoji.js

---
+++ shared/emoji.js
@@ -0,0 +1,22 @@
+const { parseEmoji, EmbedBuilder } = require("discord.js");
+
+const EMBED_COLORS = {
+  BOT_EMBED: '#5865F2'
+};
+
+module.exports = (emoji) => {
+  let custom = parseEmoji(emoji);
+  if (!custom.id) return "This is not a valid guild emoji";
+
+  let url = `https://cdn.discordapp.com/emojis/${custom.id}.${custom.animated ? "gif?v=1" : "png"}`;
+
+  const embed = new EmbedBuilder()
+    .setColor(EMBED_COLORS.BOT_EMBED)
+    .setAuthor({ name: "Emoji Info" })
+    .setDescription(
+      `**Id:** ${custom.id}\n` + `**Name:** ${custom.name}\n` + `**Animated:** ${custom.animated ? "Yes" : "No"}`
+    )
+    .setImage(url);
+
+  return { embeds: [embed] };
+};
